d1a2fa8d948674d6a062b7c51d0c2244700e641f
jsivwri <jsivwri@gmail.com>
2020-06-16 18:43:08 +0100

    Fix a bug which misidentified some unival trees

--- python/day_008.py
+++ python/day_008.py
@@ -17,24 +17,33 @@
 #  1   1
 
 
-def unival_count(root):
+def unival_count(root, recursive_call="No"):
     
     count = 0
+    return_val_L = return_val_R = None
+    return_val = root.val
+
+    if root.left == None and root.right == None: # leaf nodes can return immediately
+        count = 1
+        return count, root.val
 
     if root.left != None:
-        count += unival_count(root.left)
+        return_count, return_val_L = unival_count(root.left,"yes")
+        count += return_count
     
     if root.right != None:
-        count += unival_count(root.right)
-
-    if root.left != None and root.right != None:
-        if root.left.val == root.right.val:
-            count += 1
-
-    if root.left == root.right:
-        count +=1
+        return_count, return_val_R = unival_count(root.right,"yes")
+        count += return_count
     
-    return count 
+    if return_val_L == return_val_R and return_val_L == root.val:
+        count += 1
+    else:
+        return_val = None #'poisions the well' so that parent nodes can't be considered to unival
+
+    if recursive_call == "No":
+        return count
+    else:
+        return count, return_val
 
 
 #TEST CASE:
@@ -55,9 +64,42 @@ class Node:
         else: 
             raise TypeError('Right Node not Class Node')
 
-#build the test tree
+#build the given test tree
 left = Node(1)
 right = Node(0, Node(1,Node(1),Node(1)), Node(0))
 root = Node(0, left, right)
 
 assert unival_count(root) == 5
+
+
+#build another test tree which minimises the number of unival trees:
+#    0
+#   / \
+#  0   0
+#     / \
+#    0   0
+#   / \
+#  1   1
+
+left = Node(0)
+right = Node(0, Node(0,Node(1),Node(1)), Node(0))
+root = Node(0, left, right)
+
+assert unival_count(root) == 4
+
+
+#build another test tree which maximuses the number of unival trees:
+#    0
+#   / \
+#  0   0
+#     / \
+#    0   0
+#   / \
+#  0   0
+
+left = Node(0)
+right = Node(0, Node(0,Node(0),Node(0)), Node(0))
+root = Node(0, left, right)
+
+assert unival_count(root) == 7
+
