90e9525d990df39758ed28c81d4c5091877b76be
jsivwri <jsivwri@gmail.com>
2020-07-15 18:55:21 +0200

    Add Day 37

---
+++ python/day_037.py
@@ -0,0 +1,22 @@
+# JP SIVERTSEN-WRIGHT - DAILY CODING PROBLEM 37
+
+# This problem was asked by Google.
+
+# The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+
+# For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+
+# You may also use a list or array to represent a set.
+
+def generate_powerset(set):
+
+    output = [[]]
+
+    for val in set:
+        new_set = [item + [val] for item in output]
+        output += new_set
+        
+    output.sort(key=len)
+    return output
+    
+assert generate_powerset([1, 2, 3]) == [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
